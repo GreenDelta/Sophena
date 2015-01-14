adfb430cdd3f9ea8cc3219b9ec9e605d196ac9be
msrocka <michael.srocka@gmail.com>
2015-01-14 16:01:09 +0100

    added consumer and pump icons

--- icons/build.bat
+++ icons/build.bat
@@ -1,1 +1,3 @@
 START inkscape -z -e png\producer.png -w 16 -h 16 svg\producer.svg
+START inkscape -z -e png\consumer.png -w 16 -h 16 svg\consumer.svg
+START inkscape -z -e png\pump.png -w 16 -h 16 svg\pump.svg
