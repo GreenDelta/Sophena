be34ecb70d29740f5093d58092856a7a13ea33a5
msrocka <michael.srocka@gmail.com>
2015-02-23 17:57:46 +0100

    updated consumer icon

--- icons/build.bat
+++ icons/build.bat
@@ -1,3 +1,3 @@
-START inkscape -z -e png\producer.png -w 16 -h 16 svg\producer.svg
-START inkscape -z -e png\consumer.png -w 16 -h 16 svg\consumer.svg
-START inkscape -z -e png\pump.png -w 16 -h 16 svg\pump.svg
+START inkscape -z -e png\producer_16.png -w 16 -h 16 svg\producer.svg
+START inkscape -z -e png\consumer_16.png -w 16 -h 16 svg\consumer.svg
+START inkscape -z -e png\pump_16.png -w 16 -h 16 svg\pump.svg
