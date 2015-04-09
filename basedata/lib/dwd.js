e1e7a0915454971974820ceb7f4637fbe721ef6d
msrocka <michael.srocka@gmail.com>
2015-04-09 15:07:49 +0200

    added reader to dwd module

--- basedata/lib/dwd.js
+++ basedata/lib/dwd.js
@@ -2,31 +2,111 @@
 
 var fs = require('fs'),
 	byline = require('byline'),
-	S = require('string');
+	S = require('string'),
+	EventEmitter = require('events').EventEmitter,
+	util = require('util');
 
-function readMetaDataFile(filePath, stationCb, endCb) {
+var Reader = function() {
+	EventEmitter.call(this);
+};
+util.inherits(Reader, EventEmitter);
+
+Reader.prototype.readFolder = function(folderPath) {
+	var self = this;
+	this.emit('start-folder', folderPath);
+	fs.readdir(folderPath, function(err, fileNames) {
+		if (err)
+			return self.emit('error', err);
+
+		var metaFiles = filterFiles(fileNames, 'bestand.txt');
+		var dataFiles = filterFiles(fileNames, 'elc.txt');
+		var count = metaFiles.length + dataFiles.length;
+		if (count === 0)
+			return self.emit('end-folder', folderPath);
+
+		var handledCount = 0;
+
+		self.on('end-file', function(filePath) {
+			handledCount++;
+			if (handledCount === count) {
+				self.emit('end-folder', folderPath);
+			}
+		});
+
+		metaFiles.forEach(function(metaFile) {
+			self.readMetaFile(folderPath + '/' + metaFile);
+		});
+		dataFiles.forEach(function(dataFile) {
+			self.readDataFile(folderPath + '/' + dataFile);
+		});
+	});
+};
+
+Reader.prototype.readMetaFile = function(filePath) {
+	var self = this;
+	this.emit('start-file', filePath);
+	console.log('read meta data file:', filePath);
 
 	var fsStream = fs.createReadStream(filePath),
 		stream = byline.createStream(fsStream);
 
 	stream.on('readable', function() {
-		if (!stationCb)
-			return;
 		var buffer;
 		while ((buffer = stream.read()) !== null) {
 			var line = S(buffer.toString()).trim();
 			if (line.startsWith('Station= ')) {
 				var station = parseStationInfo(line);
-				stationCb(station)
+				self.emit('station', station);
 			}
-		};
+		}
 	});
 
 	stream.on('end', function() {
-		if (!endCb)
-			return;
-		endCb();
+		console.log('finished meta file', filePath);
+		self.emit('end-file', filePath);
 	});
+
+	stream.on('error', function(err) {
+		self.emit('error', e);
+	});
+};
+
+Reader.prototype.readDataFile = function(filePath) {
+	var self = this;
+	this.emit('start-file', filePath);
+	console.log('read data file:', filePath);
+
+	var fsStream = fs.createReadStream(filePath),
+		stream = byline.createStream(fsStream);
+
+	stream.on('readable', function() {
+		var buffer;
+		while ((buffer = stream.read()) !== null) {
+			var line = buffer.toString();
+			var record = parseDataLine(line);
+			if(record)
+				self.emit('record', record);			
+		}
+	});
+
+	stream.on('end', function() {
+		console.log('finished data file', filePath);
+		self.emit('end-file', filePath);
+	});
+
+	stream.on('error', function(err) {
+		self.emit('error', e);
+	});	
+};
+
+function filterFiles(fileNames, suffix) {
+	var files = [];
+	for (var i = 0; i < fileNames.length; i++) {
+		var name = S(fileNames[i]);
+		if (name.endsWith(suffix))
+			files.push(name.s);
+	}
+	return files;
 }
 
 function parseStationInfo(line) {
@@ -40,6 +120,59 @@ function parseStationInfo(line) {
 	};
 }
 
+function parseDataLine(line) {
+	var feed = line.trim(),
+        station = feed.substring(0, 5).trim(),
+        year = parseInt(feed.substring(5, 9).trim(), 10),
+        hour = feed.substring(9, 15),
+        val = parseFloat(feed.substring(15).trim());
+    if (val != -999)
+        val /= 10;
+    return {
+    	station: station,
+    	year: year,
+    	hour: hour,
+    	value: val
+    };
+}
+
+module.exports.Reader = Reader; 
+
+var r = new Reader();
+r.on('file', function(file) {
+	console.log(file);
+});
+r.on('error', function(err) {
+	console.log(err);
+});
+r.on('end-folder', function(path) {
+	console.log('finished folder', path);
+});
+
+
+r.on('station', function(station) {
+	// console.log('station: ', station);
+});
+
+var recordCount = 0;
+
+r.on('record', function(record) {
+	recordCount++;
+	if(recordCount % 10000 === 0) {
+		console.log(recordCount, "records handled");
+		console.log('last record: ', record);
+	}
+});
+
+r.readFolder('C:/Users/Besitzer/Projects/sophena/repo/basedata/climate_data/raw_data');
+
+
+//var folder = 'C:/Users/Besitzer/Projects/sophena/repo/basedata/climate_data/raw_data';
+//readFolder(folder);
+
+
+
+/*
 var metaFile = 'C:/Users/Besitzer/Projects/sophena/repo/basedata/climate_data/raw_data/sy_ds_abgabe011_15_1bestand.txt';
 
 readMetaDataFile(metaFile, function(station) {
@@ -47,3 +180,4 @@ readMetaDataFile(metaFile, function(station) {
 }, function() {
 	console.log('all done');
 });
+*/
