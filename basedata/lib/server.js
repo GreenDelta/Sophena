698b840842aa2edcf051b82b19805baf1dccc584
msrocka <michael.srocka@gmail.com>
2015-04-10 17:09:38 +0200

    added data service

--- basedata/lib/server.js
+++ basedata/lib/server.js
@@ -1,17 +1,51 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+	mysql = require('mysql');
+
+var app = express(),
+	pool = mysql.createPool({
+		connectionLimit: 10,
+		host: 'localhost',
+		user: 'root',
+		database: 'climatedb',
+		port: 3306
+	});
 
 app.use(express.static('assets'));
 
-app.get('/hello', function (req, res) {
-  res.send('Hello World!');
+app.get('/stations', function(req, res) {
+	var sql = 'SELECT id, name FROM tbl_stations';
+	pool.query(sql, function(err, rows, fields) {
+		if (err)
+			throw err;
+		res.json(rows);
+	});
 });
 
-var server = app.listen(8080, function () {
+app.get('/station/:id', function(req, res) {
+	var id = req.params['id'];
+	var sql = 'SELECT id, name FROM tbl_stations WHERE id = ?';
+	pool.query(sql, [id], function(err, rows, fields) {
+		if (err)
+			throw err;
+		res.json(rows[0]);
+	});
+});
 
-  var host = server.address().address;
-  var port = server.address().port;
+app.get('/data/:station', function(req, res) {
+	var station = req.params['station'];
+	var sql = 'SELECT year, hour, temperature as val from tbl_data where f_station=?';
+	pool.query(sql, [station], function(err, rows, fields) {
+		if(err)
+			throw err;
+		res.json(rows);
+	});
+});
 
-  console.log('Example app listening at http://%s:%s', host, port);
+var server = app.listen(8080, function() {
+
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('dwdat listening at %s', port);
 
 });
