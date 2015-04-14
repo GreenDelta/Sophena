1294fba639674e703ef86e495e8d7944bab9fe34
msrocka <michael.srocka@gmail.com>
2015-04-14 18:22:26 +0200

    added fuel data + packaging

---
+++ basedata/scripts/data_package.js
@@ -0,0 +1,20 @@
+var fs = require('fs'),
+	archiver = require('archiver');
+
+var output = fs.createWriteStream('../data_package.sophena');
+var archive = archiver('zip');
+
+output.on('close', function () {
+    console.log(archive.pointer() + ' total bytes');
+    console.log('archiver has been finalized and the output file descriptor has closed.');
+});
+
+archive.on('error', function(err){
+    throw err;
+});
+
+archive.pipe(output);
+archive.bulk([
+    { expand: true, cwd: '../data', src: ['**'], dest: 'sophena'}
+]);
+archive.finalize();
