b9ce4390b28113591d8b6011041d3cf158fa5657
msrocka <michael.srocka@gmail.com>
2015-04-23 15:38:16 +0200

    added logging configuration

--- sophena/src/sophena/rcp/Activator.java
+++ sophena/src/sophena/rcp/Activator.java
@@ -2,15 +2,13 @@ package sophena.rcp;
 
 import java.io.File;
 
-import org.apache.log4j.ConsoleAppender;
-import org.apache.log4j.Level;
-import org.apache.log4j.PatternLayout;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import sophena.db.Database;
+import sophena.rcp.logging.LoggerConfig;
 import sophena.rcp.utils.Rcp;
 
 public class Activator extends AbstractUIPlugin {
@@ -25,11 +23,7 @@ public class Activator extends AbstractUIPlugin {
 
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
-		// TODO: add log file etc
-		org.apache.log4j.Logger log = org.apache.log4j.Logger.getRootLogger();
-		log.setLevel(Level.ALL);
-		log.addAppender(new ConsoleAppender(new PatternLayout(
-				"%-4r %-5p %c %x - %m%n")));
+		LoggerConfig.setUp();
 		try {
 			File workspace = Rcp.getWorkspace();
 			File dbDir = new File(workspace, "database");
---
+++ sophena/src/sophena/rcp/logging/LoggerConfig.java
@@ -0,0 +1,53 @@
+package sophena.rcp.logging;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+
+import sophena.rcp.utils.EclipseCommandLine;
+
+/**
+ * The configuration of the application logging.
+ */
+public class LoggerConfig {
+
+	public static void setUp() {
+		Logger logger = Logger.getRootLogger();
+		setLogLevel(logger);
+		HtmlLogFile.create(logger);
+		logger.addAppender(new PopupAppender());
+		addConsoleOutput(logger);
+	}
+
+	private static void addConsoleOutput(Logger logger) {
+		BasicConfigurator.configure();
+		ConsoleAppender appender = new ConsoleAppender(new PatternLayout());
+		logger.addAppender(appender);
+		appender.setTarget(ConsoleAppender.SYSTEM_OUT);
+		appender.activateOptions();
+	}
+
+	private static void setLogLevel(Logger logger) {
+		String level = EclipseCommandLine.getArg("logLevel");
+		if (level != null)
+			setLevelFromCommandLine(logger, level);
+		else
+			logger.setLevel(Level.WARN);
+	}
+
+	private static void setLevelFromCommandLine(Logger logger, String level) {
+		if (level.equalsIgnoreCase("all")) {
+			logger.setLevel(Level.ALL);
+		} else if (level.equalsIgnoreCase("error")) {
+			logger.setLevel(Level.ERROR);
+		} else if (level.equalsIgnoreCase("info")) {
+			logger.setLevel(Level.INFO);
+		} else if (level.equalsIgnoreCase("warn")) {
+			logger.setLevel(Level.WARN);
+		} else {
+			logger.setLevel(Level.INFO);
+		}
+	}
+}
