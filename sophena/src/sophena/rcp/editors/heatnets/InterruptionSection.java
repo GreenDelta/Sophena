37552335dde16b31c31bee4dc6be4aaf01ac0988
msrocka <michael.srocka@gmail.com>
2015-06-08 13:10:08 +0200

    interruption: model <-> UI

--- sophena/src/sophena/rcp/editors/heatnets/InterruptionSection.java
+++ sophena/src/sophena/rcp/editors/heatnets/InterruptionSection.java
@@ -1,9 +1,15 @@
 package sophena.rcp.editors.heatnets;
 
+import java.time.MonthDay;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import sophena.model.HeatNet;
 import sophena.rcp.M;
 import sophena.rcp.utils.Controls;
 import sophena.rcp.utils.MonthDayBox;
@@ -20,33 +26,67 @@ class InterruptionSection {
 		this.editor = editor;
 	}
 
+	private HeatNet heatNet() {
+		return editor.getHeatNet();
+	}
+
 	void create(Composite body, FormToolkit toolkit) {
 		Composite composite = UI.formSection(body, toolkit,
 				"Wärmenetz - Unterbrechung");
 		UI.gridLayout(composite, 2).horizontalSpacing = 25;
 		createCheck(toolkit, composite);
-		startBox = new MonthDayBox(M.Start, composite, toolkit);
-		startBox.onSelect((monthDay) -> {
-			System.out.println(monthDay);
-		});
-
-		endBox = new MonthDayBox(M.End, composite, toolkit);
-
-
-		endBox.onSelect((monthDay) -> {
-			System.out.println(monthDay);
-		});
+		createStartBox(toolkit, composite);
+		createEndBox(toolkit, composite);
 	}
 
 	private void createCheck(FormToolkit toolkit, Composite composite) {
 		Button check = new Button(composite, SWT.CHECK);
 		check.setText("Mit Unterbrechung");
+		check.setSelection(heatNet().isWithInterruption());
 		Controls.onSelect(check, (e) -> {
 			boolean enabled = check.getSelection();
+			heatNet().setWithInterruption(enabled);
 			startBox.setEnabled(enabled);
 			endBox.setEnabled(enabled);
+			editor.setDirty();
 		});
 		UI.formLabel(composite, toolkit, "");
+	}
+
+	private void createStartBox(FormToolkit toolkit, Composite composite) {
+		startBox = new MonthDayBox(M.Start, composite, toolkit);
+		startBox.setEnabled(heatNet().isWithInterruption());
+		initBoxValue(startBox, heatNet().getInterruptionStart());
+		startBox.onSelect((monthDay) -> {
+			if (monthDay == null)
+				return;
+			heatNet().setInterruptionStart(monthDay.toString());
+			editor.setDirty();
+		});
+	}
+
+	private void createEndBox(FormToolkit toolkit, Composite composite) {
+		endBox = new MonthDayBox(M.End, composite, toolkit);
+		endBox.setEnabled(heatNet().isWithInterruption());
+		initBoxValue(endBox, heatNet().getInterruptionEnd());
+		endBox.onSelect((monthDay) -> {
+			if (monthDay == null)
+				return;
+			heatNet().setInterruptionEnd(monthDay.toString());
+			editor.setDirty();
+		});
+	}
+
+	private void initBoxValue(MonthDayBox box, String monthDay) {
+		if (box == null || monthDay == null)
+			return;
+		try {
+			MonthDay value = MonthDay.parse(monthDay);
+			box.select(value);
+		} catch (Exception e) {
+			Logger log = LoggerFactory.getLogger(getClass());
+			log.error("failed to parse MonthDay " + monthDay, e);
+		}
 	}
 
 }
