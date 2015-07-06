cdd6578cd3e5ffd10157af2d790e68a9f0431599
msrocka <michael.srocka@gmail.com>
2015-07-06 17:13:10 +0200

    :bug: fixed error in package script when package does not exist

--- basedata/scripts/data_package.js
+++ basedata/scripts/data_package.js
@@ -1,46 +1,62 @@
 var fs = require('fs'),
-	archiver = require('archiver');
+    archiver = require('archiver');
 
 var file = '../data_package.sophena';
 
-fs.unlink(file, function(err) {
-	if(err) {
-		console.log('failed to delete ' + file);
-		throw err;
-	}
-
-	var output = fs.createWriteStream(file);
-	var archive = archiver('zip');
-
-	output.on('close', function() {
-		console.log(archive.pointer() + ' total bytes');
-		console.log('package written: ' + file);
-	});
-
-	archive.on('error', function(err) {
-		throw err;
-	});
-
-	archive.pipe(output);
-	archive.bulk([{
-		expand: true,
-		cwd: '../data/fuels',
-		src: ['**'],
-		dest: 'fuels'
-	}]);
-	archive.bulk([{
-		expand: true,
-		cwd: '../data/boilers',
-		src: ['**'],
-		dest: 'boilers'
-	}]);
-	archive.bulk([{
-		expand: true,
-		cwd: '../climate_data/out/json',
-		src: ['**'],
-		dest: 'weather_stations'
-	}]);
-	archive.finalize();
-
-
+fs.stat(file, function(err, stat) {
+    if (err == null) {
+        deleteAndPack(file);
+    } else if (err.code == 'ENOENT') {
+        // file does not exists
+        packData(file);
+    } else {
+        // some other error
+        throw err;
+    }
 });
+
+function deleteAndPack(file) {
+    console.log('File exists; delete old version');
+    fs.unlink(file, function(err) {
+        if (err) {
+            console.log('failed to delete ' + file);
+            throw err;
+        }
+        packData(file);
+    });
+}
+
+function packData(file) {
+    var output = fs.createWriteStream(file);
+    var archive = archiver('zip');
+
+    output.on('close', function() {
+        console.log(archive.pointer() + ' total bytes');
+        console.log('package written: ' + file);
+    });
+
+    archive.on('error', function(err) {
+        throw err;
+    });
+
+    archive.pipe(output);
+    archive.bulk([{
+        expand: true,
+        cwd: '../data/fuels',
+        src: ['**'],
+        dest: 'fuels'
+    }]);
+    archive.bulk([{
+        expand: true,
+        cwd: '../data/boilers',
+        src: ['**'],
+        dest: 'boilers'
+    }]);
+    archive.bulk([{
+        expand: true,
+        cwd: '../climate_data/out/json',
+        src: ['**'],
+        dest: 'weather_stations'
+    }]);
+    archive.finalize();
+}
