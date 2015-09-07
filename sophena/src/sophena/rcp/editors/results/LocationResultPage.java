6aed8104549f13e2362de4833a373987b191533f
msrocka <michael.srocka@gmail.com>
2015-09-07 16:20:12 +0200

    set result data in LocationResultPage

--- sophena/src/sophena/rcp/editors/results/LocationResultPage.java
+++ sophena/src/sophena/rcp/editors/results/LocationResultPage.java
@@ -3,7 +3,8 @@ package sophena.rcp.editors.results;
 import java.io.File;
 import java.io.InputStream;
 import java.nio.file.Files;
-import java.util.function.Supplier;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.browser.Browser;
@@ -21,23 +22,21 @@ import org.slf4j.LoggerFactory;
 
 import com.google.gson.Gson;
 
-import sophena.model.Project;
+import sophena.model.Consumer;
+import sophena.model.Location;
 import sophena.rcp.M;
 import sophena.rcp.utils.Rcp;
 import sophena.rcp.utils.UI;
 
 public class LocationResultPage extends FormPage {
 
-	private Supplier<Project> proj;
 	private ResultEditor editor;
 	private FormToolkit toolkit;
 	private Browser browser;
 
-	public LocationResultPage(ResultEditor editor) {// ,
-
+	public LocationResultPage(ResultEditor editor) {
 		super(editor, "sophena.LocationResultPage.html", "Standort");
 		this.editor = editor;
-
 	}
 
 	@Override
@@ -83,27 +82,63 @@ public class LocationResultPage extends FormPage {
 	}
 
 	private void initBrowser() {
-		// new LocationChangeFn();
-		InitData initData = new InitData();
-
-		String json = new Gson().toJson(initData);
+		String json = createModel();
 		try {
-			browser.evaluate("init(" + json + ")");
+			System.out.println(json);
+			browser.evaluate("setData(" + json + ")");
 		} catch (Exception e) {
 			Logger log = LoggerFactory.getLogger(getClass());
 			log.error("failed to initialize browser " + json, e);
 		}
 	}
 
-	@SuppressWarnings("unused")
-	private class InitData {
-		boolean withMarker;
-		final LatLng latlng = new LatLng();
+	private String createModel() {
+		if (editor.project == null)
+			return "{}";
+		MapData data = new MapData();
+		int points = 0;
+		double latSum = 0;
+		double lngSum = 0;
+		for (Consumer c : editor.project.consumers) {
+			Location loc = c.location;
+			if (loc == null)
+				continue;
+			points++;
+			latSum += loc.latitude;
+			lngSum += loc.longitude;
+			Marker marker = new Marker();
+			marker.title = c.name;
+			marker.latlng = new LatLng(loc.latitude, loc.longitude);
+			data.markers.add(marker);
+		}
+		if (points == 0)
+			data.center = new LatLng(48.884, 12.583);
+		else
+			data.center = new LatLng(latSum / points, lngSum / points);
+		return new Gson().toJson(data);
 	}
 
+	@SuppressWarnings("unused")
+	private class MapData {
+		LatLng center;
+		List<Marker> markers = new ArrayList<>();
+	}
+
+	@SuppressWarnings("unused")
+	private class Marker {
+		String title;
+		LatLng latlng;
+	}
+
+	@SuppressWarnings("unused")
 	private class LatLng {
 		double lat;
 		double lng;
+
+		LatLng(double lat, double lng) {
+			this.lat = lat;
+			this.lng = lng;
+		}
 	}
 
 }
