a93d66909b5c249d510e2b5687adcee58ab5060d
msrocka <michael.srocka@gmail.com>
2015-10-20 15:31:55 +0200

    smaller refactorings

--- sophena/src/sophena/rcp/editors/results/compare/HeatCostsChart.java
+++ sophena/src/sophena/rcp/editors/results/compare/HeatCostsChart.java
@@ -13,6 +13,7 @@ import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.ui.forms.widgets.Section;
 
 import sophena.calc.Comparison;
+import sophena.calc.CostResult;
 import sophena.calc.ProjectResult;
 import sophena.model.Stats;
 import sophena.rcp.utils.Colors;
@@ -38,6 +39,14 @@ class HeatCostsChart {
 		graph = new XYGraph();
 		lws.setContents(graph);
 		graph.setShowTitle(false);
+		configureAxis();
+		for (int i = 0; i < comparison.projects.length; i++) {
+			addNormalTrace(i);
+			addFundingTrace(i);
+		}
+	}
+
+	private void configureAxis() {
 		Axis x = graph.primaryXAxis;
 		Axis y = graph.primaryYAxis;
 		x.setRange(0, comparison.projects.length * 30);
@@ -45,22 +54,18 @@ class HeatCostsChart {
 		double max = 0;
 		for (int i = 0; i < comparison.projects.length; i++) {
 			ProjectResult pr = comparison.results[i];
-			max = Math.max(max, pr.costResult.brutto.heatGenerationCosts * 1000);
-			max = Math.max(max, pr.costResultFunding.brutto.heatGenerationCosts * 1000);
+			max = Math.max(max, value(pr.costResult));
+			max = Math.max(max, value(pr.costResultFunding));
 		}
 		y.setRange(0, Stats.nextStep(max, 10));
 		y.setTitleFont(y.getFont());
 		y.setTitle("EUR/MWh");
 		y.setShowMajorGrid(true);
-		for (int i = 0; i < comparison.projects.length; i++) {
-			addNormalTrace(i);
-			addFundingTrace(i);
-		}
 	}
 
 	private void addNormalTrace(int i) {
 		double x = i * 30 + 10;
-		double y = comparison.results[i].costResult.brutto.heatGenerationCosts * 1000;
+		double y = value(comparison.results[i].costResult);
 		String label = comparison.projects[i].name;
 		Trace trace = createTrace(label, x, y);
 		trace.setAreaAlpha(255);
@@ -70,7 +75,7 @@ class HeatCostsChart {
 
 	private void addFundingTrace(int i) {
 		double x = i * 30 + 20;
-		double y = comparison.results[i].costResultFunding.brutto.heatGenerationCosts * 1000;
+		double y = value(comparison.results[i].costResultFunding);
 		String label = comparison.projects[i].name + " - mit Förderung";
 		Trace trace = createTrace(label, x, y);
 		trace.setAreaAlpha(100);
@@ -83,11 +88,18 @@ class HeatCostsChart {
 		data.setBufferSize(1);
 		data.setCurrentXData(x);
 		data.setCurrentYData(y);
-		Trace trace = new Trace(label, graph.primaryXAxis, graph.primaryYAxis,
-				data);
+		Trace trace = new Trace(label, graph.primaryXAxis,
+				graph.primaryYAxis, data);
 		trace.setTraceType(TraceType.BAR);
 		trace.setLineWidth(20);
 		return trace;
+	}
+
+	private double value(CostResult result) {
+		if (result == null || result.brutto == null)
+			return 0;
+		double val = result.brutto.heatGenerationCosts;
+		return val * 1000;
 	}
 
 }
