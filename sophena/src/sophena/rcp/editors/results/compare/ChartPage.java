34d9e3604462be397af9f54cf75bb4cd1637d0d3
msrocka <michael.srocka@gmail.com>
2015-10-20 15:16:55 +0200

    chart for heat costs comparison

--- sophena/src/sophena/rcp/editors/results/compare/ChartPage.java
+++ sophena/src/sophena/rcp/editors/results/compare/ChartPage.java
@@ -1,19 +1,10 @@
 package sophena.rcp.editors.results.compare;
 
-import org.eclipse.draw2d.LightweightSystem;
-import org.eclipse.nebula.visualization.xygraph.dataprovider.CircularBufferDataProvider;
-import org.eclipse.nebula.visualization.xygraph.figures.Trace;
-import org.eclipse.nebula.visualization.xygraph.figures.Trace.TraceType;
-import org.eclipse.nebula.visualization.xygraph.figures.XYGraph;
-import org.eclipse.nebula.visualization.xygraph.util.XYGraphMediaFactory;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.forms.IManagedForm;
 import org.eclipse.ui.forms.editor.FormPage;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.ui.forms.widgets.ScrolledForm;
-import org.eclipse.ui.forms.widgets.Section;
 
 import sophena.calc.Comparison;
 import sophena.rcp.utils.UI;
@@ -32,36 +23,8 @@ class ChartPage extends FormPage {
 		ScrolledForm form = UI.formHeader(mform, "Ergebnisse - Energie");
 		FormToolkit tk = mform.getToolkit();
 		Composite body = UI.formBody(form, tk);
-
-		Section section = UI.section(body, tk, "Wärmegestehungskosten");
-		UI.gridData(section, true, false);
-		Composite composite = UI.sectionClient(section, tk);
-		UI.gridLayout(composite, 1);
-		Canvas canvas = new Canvas(composite, SWT.DOUBLE_BUFFERED);
-		UI.gridData(canvas, true, true).minimumHeight = 250;
-		LightweightSystem lws = new LightweightSystem(canvas);
-		XYGraph g = new XYGraph();
-		lws.setContents(g);
-		g.setShowTitle(false);
-
-		CircularBufferDataProvider data = new CircularBufferDataProvider(false);
-		data.setBufferSize(4);
-		data.setCurrentXDataArray(new double[] { 1, 3, 5, 7 });
-		data.setCurrentYDataArray(new double[] { 10, 20, 40, 50 });
-
-		Trace trace = new Trace(
-				"Trace1-XY Plot",
-				g.primaryXAxis,
-				g.primaryYAxis,
-				data);
-
-		// set trace property
-		trace.setTraceType(TraceType.BAR);
-		trace.setLineWidth(15);
-		trace.setAreaAlpha(200);
-		trace.setTraceColor(XYGraphMediaFactory.getInstance().getColor(XYGraphMediaFactory.COLOR_BLUE));
-		// add the trace to xyGraph
-		g.addTrace(trace);
+		new HeatCostsChart(comparison).create(body, tk);
 
 	}
+
 }
---
+++ sophena/src/sophena/rcp/editors/results/compare/HeatCostsChart.java
@@ -0,0 +1,93 @@
+package sophena.rcp.editors.results.compare;
+
+import org.eclipse.draw2d.LightweightSystem;
+import org.eclipse.nebula.visualization.xygraph.dataprovider.CircularBufferDataProvider;
+import org.eclipse.nebula.visualization.xygraph.figures.Axis;
+import org.eclipse.nebula.visualization.xygraph.figures.Trace;
+import org.eclipse.nebula.visualization.xygraph.figures.Trace.TraceType;
+import org.eclipse.nebula.visualization.xygraph.figures.XYGraph;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.eclipse.ui.forms.widgets.Section;
+
+import sophena.calc.Comparison;
+import sophena.calc.ProjectResult;
+import sophena.model.Stats;
+import sophena.rcp.utils.Colors;
+import sophena.rcp.utils.UI;
+
+class HeatCostsChart {
+
+	private Comparison comparison;
+	private XYGraph graph;
+
+	HeatCostsChart(Comparison comparison) {
+		this.comparison = comparison;
+	}
+
+	void create(Composite body, FormToolkit tk) {
+		Section section = UI.section(body, tk, "Wärmegestehungskosten in EUR/MWh");
+		UI.gridData(section, true, false);
+		Composite composite = UI.sectionClient(section, tk);
+		UI.gridLayout(composite, 1);
+		Canvas canvas = new Canvas(composite, SWT.DOUBLE_BUFFERED);
+		UI.gridData(canvas, true, true).minimumHeight = 250;
+		LightweightSystem lws = new LightweightSystem(canvas);
+		graph = new XYGraph();
+		lws.setContents(graph);
+		graph.setShowTitle(false);
+		Axis x = graph.primaryXAxis;
+		Axis y = graph.primaryYAxis;
+		x.setRange(0, comparison.projects.length * 30);
+		x.setVisible(false);
+		double max = 0;
+		for (int i = 0; i < comparison.projects.length; i++) {
+			ProjectResult pr = comparison.results[i];
+			max = Math.max(max, pr.costResult.brutto.heatGenerationCosts * 1000);
+			max = Math.max(max, pr.costResultFunding.brutto.heatGenerationCosts * 1000);
+		}
+		y.setRange(0, Stats.nextStep(max, 10));
+		y.setTitleFont(y.getFont());
+		y.setTitle("EUR/MWh");
+		y.setShowMajorGrid(true);
+		for (int i = 0; i < comparison.projects.length; i++) {
+			addNormalTrace(i);
+			addFundingTrace(i);
+		}
+	}
+
+	private void addNormalTrace(int i) {
+		double x = i * 30 + 10;
+		double y = comparison.results[i].costResult.brutto.heatGenerationCosts * 1000;
+		String label = comparison.projects[i].name;
+		Trace trace = createTrace(label, x, y);
+		trace.setAreaAlpha(255);
+		trace.setTraceColor(Colors.getForChart(i));
+		graph.addTrace(trace);
+	}
+
+	private void addFundingTrace(int i) {
+		double x = i * 30 + 20;
+		double y = comparison.results[i].costResultFunding.brutto.heatGenerationCosts * 1000;
+		String label = comparison.projects[i].name + " - mit Förderung";
+		Trace trace = createTrace(label, x, y);
+		trace.setAreaAlpha(100);
+		trace.setTraceColor(Colors.getForChart(i));
+		graph.addTrace(trace);
+	}
+
+	private Trace createTrace(String label, double x, double y) {
+		CircularBufferDataProvider data = new CircularBufferDataProvider(false);
+		data.setBufferSize(1);
+		data.setCurrentXData(x);
+		data.setCurrentYData(y);
+		Trace trace = new Trace(label, graph.primaryXAxis, graph.primaryYAxis,
+				data);
+		trace.setTraceType(TraceType.BAR);
+		trace.setLineWidth(20);
+		return trace;
+	}
+
+}
