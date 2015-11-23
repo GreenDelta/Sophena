ad0808e64ff7618dbcdd4f3a648e80187d27f66f
msrocka <michael.srocka@gmail.com>
2015-11-23 11:33:53 +0100

    extract default database if database not exists

--- sophena/src/sophena/rcp/Activator.java
+++ sophena/src/sophena/rcp/Activator.java
@@ -1,11 +1,17 @@
 package sophena.rcp;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 
+import org.apache.commons.io.IOUtils;
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.zeroturnaround.zip.ZipUtil;
 
 import sophena.db.Database;
 import sophena.rcp.logging.LoggerConfig;
@@ -27,10 +33,28 @@ public class Activator extends AbstractUIPlugin {
 		try {
 			File workspace = Rcp.getWorkspace();
 			File dbDir = new File(workspace, "database");
+			if (!dbDir.exists())
+				extractDefaultDatabase(workspace);
 			Database db = new Database(dbDir);
 			App.init(db);
 		} catch (Exception e) {
 			log.error("database initialization failed", e);
+		}
+	}
+
+	private void extractDefaultDatabase(File workspace) {
+		try {
+			InputStream zipStream = FileLocator.openStream(getBundle(),
+					new Path("resources/database.zip"), false);
+			File zipFile = new File(workspace, "@temp.zip");
+			try (FileOutputStream out = new FileOutputStream(zipFile)) {
+				IOUtils.copy(zipStream, out);
+			}
+			ZipUtil.unpack(zipFile, workspace);
+			if (!zipFile.delete())
+				zipFile.deleteOnExit();
+		} catch (Exception e) {
+			log.error("failed to extract default database", e);
 		}
 	}
 
