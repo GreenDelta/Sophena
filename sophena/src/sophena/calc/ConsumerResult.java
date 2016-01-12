5bedf56cef6ff9b0372cb60d0b4c8c3ecc43b6ea
msrocka <michael.srocka@gmail.com>
2016-01-12 14:24:42 +0100

    calculate consumer results

---
+++ sophena/src/sophena/calc/ConsumerResult.java
@@ -0,0 +1,10 @@
+package sophena.calc;
+
+import sophena.model.Consumer;
+
+public class ConsumerResult {
+
+	public Consumer consumer;
+	public double heatDemand;
+
+}
--- sophena/src/sophena/calc/ProjectResult.java
+++ sophena/src/sophena/calc/ProjectResult.java
@@ -1,6 +1,12 @@
 package sophena.calc;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import sophena.model.Consumer;
+import sophena.model.LoadProfile;
 import sophena.model.Project;
+import sophena.model.Stats;
 
 public class ProjectResult {
 
@@ -8,18 +14,41 @@ public class ProjectResult {
 	public CostResult costResult;
 	public CostResult costResultFunding;
 
+	public double heatNetLoss;
+	public final List<ConsumerResult> consumerResults = new ArrayList<>();
+
 	ProjectResult() {
 	}
 
 	public static ProjectResult calculate(Project project) {
 		ProjectResult r = new ProjectResult();
+		if (project == null)
+			return r;
 		r.energyResult = EnergyCalculator.calculate(project);
 		CostCalculator costCalc = new CostCalculator(project, r.energyResult);
 		costCalc.withFunding(false);
 		r.costResult = costCalc.calculate();
 		costCalc.withFunding(true);
 		r.costResultFunding = costCalc.calculate();
+		consumers(project, r);
+		r.heatNetLoss = Stats.sum(ProjectLoad.getNetLoadCurve(project.heatNet));
 		return r;
+	}
+
+	private static void consumers(Project proj, ProjectResult r) {
+		for (Consumer consumer : proj.consumers) {
+			LoadProfile profile = ConsumerLoadCurve.calculate(consumer,
+					proj.weatherStation);
+			double[] curve = profile.calculateTotal();
+			if (proj.heatNet != null && proj.heatNet.withInterruption) {
+				ProjectLoad.applyInterruption(curve, proj.heatNet);
+			}
+			double total = Stats.sum(curve);
+			ConsumerResult result = new ConsumerResult();
+			result.consumer = consumer;
+			result.heatDemand = total;
+			r.consumerResults.add(result);
+		}
 	}
 
 }
