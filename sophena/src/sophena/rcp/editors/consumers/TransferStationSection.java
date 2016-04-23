89988b15671225b8a695d0a670c6706082138186
msrocka <michael.srocka@gmail.com>
2016-04-23 17:56:11 +0200

    added product selection to transfer station section

--- sophena/src/sophena/rcp/editors/consumers/TransferStationSection.java
+++ sophena/src/sophena/rcp/editors/consumers/TransferStationSection.java
@@ -1,11 +1,20 @@
 package sophena.rcp.editors.consumers;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.forms.events.HyperlinkAdapter;
+import org.eclipse.ui.forms.events.HyperlinkEvent;
 import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.eclipse.ui.forms.widgets.ImageHyperlink;
 
 import sophena.model.Consumer;
 import sophena.model.ProductCosts;
+import sophena.model.TransferStation;
+import sophena.rcp.Icon;
+import sophena.rcp.SearchDialog;
 import sophena.rcp.editors.ProductCostSection;
+import sophena.rcp.utils.Colors;
+import sophena.rcp.utils.DeleteLink;
 import sophena.rcp.utils.UI;
 
 class TransferStationSection {
@@ -24,6 +33,7 @@ class TransferStationSection {
 	void create(Composite body, FormToolkit tk) {
 		Composite comp = UI.formSection(body, tk, "Hausübergabestation");
 		UI.gridLayout(comp, 3);
+		createProductRow(comp, tk);
 		if (consumer().transferStationCosts == null) {
 			consumer().transferStationCosts = new ProductCosts();
 		}
@@ -32,4 +42,51 @@ class TransferStationSection {
 				.createFields(comp, tk);
 	}
 
+	private void createProductRow(Composite comp, FormToolkit tk) {
+		UI.formLabel(comp, tk, "Produkt");
+		Composite inner = tk.createComposite(comp);
+		UI.innerGrid(inner, 2);
+		ImageHyperlink link = new ImageHyperlink(inner, SWT.TOP);
+		if (consumer().transferStation != null) {
+			link.setText(consumer().transferStation.name);
+		} else {
+			link.setText("(keine Hausübergabestation ausgewählt)");
+		}
+		link.setImage(Icon.CONSUMER_16.img());
+		link.setForeground(Colors.getLinkBlue());
+		link.addHyperlinkListener(new HyperlinkAdapter() {
+			@Override
+			public void linkActivated(HyperlinkEvent e) {
+				selectTransferStation(link);
+			}
+		});
+		createDeleteLink(inner, link);
+		UI.formLabel(comp, tk, "");
+	}
+
+	private void createDeleteLink(Composite comp, ImageHyperlink link) {
+		DeleteLink.on(comp, () -> {
+			if (consumer().transferStation == null)
+				return;
+			consumer().transferStation = null;
+			link.setText("(keine Hausübergabestation ausgewählt)");
+			link.getParent().pack();
+			editor.setDirty();
+		});
+	}
+
+	private void selectTransferStation(ImageHyperlink link) {
+		TransferStation s = SearchDialog.open("Hausübergabestation",
+				TransferStation.class);
+		if (s == null)
+			return;
+		consumer().transferStation = s;
+		link.setText(s.name);
+		link.getParent().pack();
+		ProductCosts costs = consumer().transferStationCosts;
+		ProductCosts.copy(s.group, costs);
+		if (s.purchasePrice != null)
+			costs.investment = s.purchasePrice;
+		costSection.refresh();
+	}
 }
