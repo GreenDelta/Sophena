d41d268e3d0c39f1ae99050bf9b231bbbe6f7843
msrocka <michael.srocka@gmail.com>
2016-05-09 14:57:25 +0200

    added method for getting cleaning descriptors

--- sophena/src/sophena/db/daos/ProjectDao.java
+++ sophena/src/sophena/db/daos/ProjectDao.java
@@ -6,6 +6,7 @@ import java.util.List;
 import sophena.db.Database;
 import sophena.db.NativeSql;
 import sophena.model.Project;
+import sophena.model.descriptors.CleaningDescriptor;
 import sophena.model.descriptors.ProjectDescriptor;
 
 public class ProjectDao extends RootEntityDao<Project> {
@@ -28,6 +29,30 @@ public class ProjectDao extends RootEntityDao<Project> {
 			});
 		} catch (Exception e) {
 			log.error("failed to get descriptors for " + getType(), e);
+		}
+		return list;
+	}
+
+	/**
+	 * Returns the descriptors of the flue gas cleaning facilities of a project.
+	 */
+	public List<CleaningDescriptor> getCleaningDescriptors(String projectId) {
+		String sql = "SELECT e.id, c.name, c.description FROM "
+				+ "tbl_flue_gas_cleaning_entries e INNER JOIN "
+				+ "tbl_flue_gas_cleaning c ON e.f_flue_gas_cleaning = c.id "
+				+ "WHERE e.f_project = '" + projectId + "'";
+		List<CleaningDescriptor> list = new ArrayList<>();
+		try {
+			NativeSql.on(db).query(sql, r -> {
+				CleaningDescriptor d = new CleaningDescriptor();
+				d.id = r.getString(1);
+				d.name = r.getString(2);
+				d.description = r.getString(3);
+				list.add(d);
+				return true;
+			});
+		} catch (Exception e) {
+			log.error("failed to get flue gas cleaning descriptors", e);
 		}
 		return list;
 	}
---
+++ sophena/src/sophena/model/descriptors/CleaningDescriptor.java
@@ -0,0 +1,12 @@
+package sophena.model.descriptors;
+
+import sophena.model.ModelType;
+
+public class CleaningDescriptor extends Descriptor {
+
+	@Override
+	public ModelType getType() {
+		return ModelType.FLUE_GAS_CLEANING;
+	}
+
+}
