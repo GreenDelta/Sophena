0c49a8aec472910c57f7bbcfc1d280a9f4674a3e
msrocka <michael.srocka@gmail.com>
2016-05-10 10:47:08 +0200

    added cleaning element to navigation

---
+++ sophena/src/sophena/rcp/navigation/CleaningElement.java
@@ -0,0 +1,39 @@
+package sophena.rcp.navigation;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.swt.graphics.Image;
+
+import sophena.model.descriptors.CleaningDescriptor;
+import sophena.model.descriptors.ProjectDescriptor;
+import sophena.rcp.Icon;
+
+public class CleaningElement extends ContentElement<CleaningDescriptor> {
+
+	public CleaningElement(NavigationElement parent, CleaningDescriptor d) {
+		super(parent, d);
+	}
+
+	public ProjectDescriptor getProject() {
+		NavigationElement parent = getParent();
+		if (parent instanceof FolderElement)
+			return ((FolderElement) parent).getProject();
+		else
+			return null;
+	}
+
+	@Override
+	public List<NavigationElement> getChilds() {
+		return Collections.emptyList();
+	}
+
+	@Override
+	public Image getImage() {
+		return Icon.FLUE_GAS_16.img();
+	}
+
+	@Override
+	public void update() {
+	}
+}
