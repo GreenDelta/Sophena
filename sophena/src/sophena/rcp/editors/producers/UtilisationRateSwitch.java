8fa8d0c2db8a5697607e6e6977e55959b42a963c
msrocka <michael.srocka@gmail.com>
2016-05-27 14:02:06 +0200

    finished utilisation rate switch for producers

--- sophena/src/sophena/rcp/editors/producers/UtilisationRateSwitch.java
+++ sophena/src/sophena/rcp/editors/producers/UtilisationRateSwitch.java
@@ -6,7 +6,12 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 
+import sophena.model.Producer;
+import sophena.rcp.utils.Colors;
+import sophena.rcp.utils.Controls;
+import sophena.rcp.utils.Texts;
 import sophena.rcp.utils.UI;
+import sophena.utils.Num;
 
 class UtilisationRateSwitch {
 
@@ -15,6 +20,10 @@ class UtilisationRateSwitch {
 
 	private UtilisationRateSwitch(ProducerEditor editor) {
 		this.editor = editor;
+	}
+
+	private Producer producer() {
+		return editor.getProducer();
 	}
 
 	public static void create(ProducerEditor editor, Composite c, FormToolkit tk) {
@@ -27,10 +36,51 @@ class UtilisationRateSwitch {
 		UI.formLabel(parent, tk, "Nutzungsgrad");
 		Composite comp = tk.createComposite(parent);
 		UI.innerGrid(comp, 2);
-		Button calc = tk.createButton(comp, "Automatische Berechnung", SWT.RADIO);
+		Button r1 = tk.createButton(comp, "Automatische Berechnung", SWT.RADIO);
 		UI.filler(comp);
-		Button enter = tk.createButton(comp, "Manuelle Eingabe", SWT.RADIO);
+		Button r2 = tk.createButton(comp, "Manuelle Eingabe", SWT.RADIO);
 		text = tk.createText(comp, "");
+		UI.gridData(text, false, false).widthHint = 80;
+		initText();
+		if (producer().utilisationRate == null) {
+			r1.setSelection(true);
+		} else {
+			r2.setSelection(true);
+		}
+		Controls.onSelect(r1, e -> switchToCalculation());
+		Controls.onSelect(r2, e -> switchToInput());
 	}
 
+	private void initText() {
+		if (text == null)
+			return;
+		if (producer().utilisationRate == null) {
+			text.setBackground(Colors.forCalculatedField());
+			text.setEditable(false);
+		} else {
+			text.setBackground(Colors.forModifiedDefault());
+			Texts.set(text, producer().utilisationRate);
+		}
+		Texts.on(text).decimal().onChanged(t -> {
+			if (producer().utilisationRate == null)
+				return;
+			producer().utilisationRate = Num.read(t);
+			editor.setDirty();
+		});
+	}
+
+	private void switchToCalculation() {
+		producer().utilisationRate = null;
+		text.setText("");
+		text.setBackground(Colors.forCalculatedField());
+		text.setEditable(false);
+		editor.setDirty();
+	}
+
+	private void switchToInput() {
+		producer().utilisationRate = 0.8;
+		Texts.set(text, 0.8);
+		text.setBackground(Colors.forModifiedDefault());
+		text.setEditable(true);
+	}
 }
