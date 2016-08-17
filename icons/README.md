6508a07243daa867adf1d82d06956622ac98dfbf
msrocka <michael.srocka@gmail.com>
2016-08-17 10:57:17 +0200

    removed old icon files

--- icons/README.md
+++
@@ -1,3 +0,0 @@
-icons
-=====
-This folder contains the icons that are created for the Sophena application.
--- icons/build.bat
+++
@@ -1,3 +0,0 @@
-START inkscape -z -e png\producer_16.png -w 16 -h 16 svg\producer.svg
-START inkscape -z -e png\consumer_16.png -w 16 -h 16 svg\consumer.svg
-START inkscape -z -e png\pump_16.png -w 16 -h 16 svg\pump.svg
