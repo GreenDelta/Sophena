19d6f41f6d36eb65d17731bf28772d171b342fda
msrocka <michael.srocka@gmail.com>
2018-01-26 13:26:30 +0100

    classpath: enable JavaFX; fix #14; new icon for required load

--- sophena/src/sophena/rcp/editors/basedata/buildings/BuildingStateEditor.java
+++ sophena/src/sophena/rcp/editors/basedata/buildings/BuildingStateEditor.java
@@ -75,7 +75,7 @@ public class BuildingStateEditor extends Editor {
 			UI.gridLayout(comp, 1);
 			TableLabel label = new TableLabel();
 			TableViewer table = Tables.createViewer(comp, "Gebäudetyp",
-					"Gebäudezustand", "Heizgrenztemperatur", "Warmwasseranteil",
+					"Gebäudezustand (unsaniert)", "Heizgrenztemperatur", "Warmwasseranteil",
 					"Volllaststunden", "Voreinstellung");
 			table.setContentProvider(new TableContent(label));
 			table.setLabelProvider(label);
