cf6dc88023e9fb561edb99e0fe3c3a35ee5ff396
msrocka <michael.srocka@gmail.com>
2018-07-09 13:07:19 +0200

    new calculation function for capital costs

--- sophena/src/sophena/math/costs/CapitalCosts.java
+++ sophena/src/sophena/math/costs/CapitalCosts.java
@@ -1,5 +1,7 @@
 package sophena.math.costs;
 
+import static java.lang.Math.pow;
+
 import sophena.calc.CostResultItem;
 import sophena.model.Project;
 
@@ -10,21 +12,62 @@ public class CapitalCosts {
 
 	public static double get(CostResultItem item, Project project,
 			double interestRate) {
-		if (item == null || item.costs == null || project == null)
+		if (item == null || item.costs == null
+				|| project == null || project.costSettings == null)
 			return 0;
-		double annuityFactor = Costs.annuityFactor(project, interestRate);
-		double residualValue = ResidualValue.get(item, project, interestRate);
-		int projectDuration = project.duration;
-		int usageDuration = item.costs.duration;
-		if (projectDuration <= usageDuration)
-			return (item.costs.investment - residualValue) * annuityFactor;
-		int replacements = Replacements.getNumber(item, project);
-		double costs = item.costs.investment;
-		for (int i = 1; i <= replacements; i++) {
-			costs += Replacements.getPresentValue(i, item, project,
-					interestRate);
+		double interestFactor = 1 + interestRate / 100;
+		double priceChangeFactor = project.costSettings.investmentFactor;
+		return calculate(
+				item.costs.investment,
+				item.costs.duration,
+				project.duration,
+				interestFactor,
+				priceChangeFactor);
+	}
+
+	/**
+	 * Low level function to calculate the capital costs of a component
+	 * according to VDI 2067.
+	 * 
+	 * @param A
+	 *            Investment amount (in EUR)
+	 * @param Tu
+	 *            service life (in years) of the component
+	 * @param T
+	 *            obervation period (in years)
+	 * @param q
+	 *            interest factor (e.g. 1.02)
+	 * @param r
+	 *            price change factor (e.g. 1.02)
+	 */
+	private static double calculate(
+			double A,
+			int Tu,
+			int T,
+			double q,
+			double r) {
+
+		double sum = A;
+
+		// add cash values of possible replacements
+		int t = Tu;
+		int n = 0; // the number of replacements
+		while (t < T) {
+			sum += A * pow(r, t) / pow(q, t);
+			n++;
+			t += Tu;
 		}
-		costs -= residualValue;
-		return costs * annuityFactor;
+
+		// remove a possible residual value
+		int Tr = Tu * (1 + n) - T;
+		if (Tr > 0) {
+			double factor = ((double) ((n + 1) * Tu - T)) / ((double) Tu);
+			double Rv = A * pow(r, n * Tu) * factor * 1 / pow(q, T);
+			sum -= Rv;
+		}
+
+		// apply the annuity factor
+		double a = (q - 1) / (1 - pow(q, -T));
+		return sum * a;
 	}
 }
