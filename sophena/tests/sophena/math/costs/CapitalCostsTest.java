ca87389d36ae089920d6473f11a37c4219eab84b
msrocka <michael.srocka@gmail.com>
2018-07-09 12:18:49 +0200

    more tests for capital costs

--- sophena/tests/sophena/math/costs/CapitalCostsTest.java
+++ sophena/tests/sophena/math/costs/CapitalCostsTest.java
@@ -6,6 +6,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import sophena.calc.CostResultItem;
+import sophena.model.CostSettings;
 import sophena.model.ProductCosts;
 import sophena.model.Project;
 
@@ -31,6 +32,74 @@ public class CapitalCostsTest {
 
 		item.costs.duration = 10;
 		Assert.assertEquals(1193.80830512335, fn.getAsDouble(), 1E-10);
+	}
+
+	@Test
+	public void testWithReplacementsWithResidualValue() {
+		Project project = new Project();
+		project.duration = 20;
+		project.costSettings = new CostSettings();
+		project.costSettings.interestRate = 2;
+		project.costSettings.investmentFactor = 1.03;
+
+		CostResultItem item = new CostResultItem();
+		item.costs = new ProductCosts();
+		item.costs.investment = 10_000;
+		item.costs.duration = 8;
+
+		double capitalCosts = CapitalCosts.get(item, project, 2);
+		Assert.assertEquals(1657.4431, capitalCosts, 1e-3);
+	}
+
+	@Test
+	public void testWithReplacementsNoResidualValue() {
+		Project project = new Project();
+		project.duration = 20;
+		project.costSettings = new CostSettings();
+		project.costSettings.interestRate = 2;
+		project.costSettings.investmentFactor = 1.03;
+
+		CostResultItem item = new CostResultItem();
+		item.costs = new ProductCosts();
+		item.costs.investment = 10_000;
+		item.costs.duration = 5;
+
+		double capitalCosts = CapitalCosts.get(item, project, 2);
+		Assert.assertEquals(2635.8927, capitalCosts, 1e-3);
+	}
+
+	@Test
+	public void testNoReplacementsWithResidualValue() {
+		Project project = new Project();
+		project.duration = 20;
+		project.costSettings = new CostSettings();
+		project.costSettings.interestRate = 2;
+		project.costSettings.investmentFactor = 1.03;
+
+		CostResultItem item = new CostResultItem();
+		item.costs = new ProductCosts();
+		item.costs.investment = 10_000;
+		item.costs.duration = 30;
+
+		double capitalCosts = CapitalCosts.get(item, project, 2);
+		Assert.assertEquals(474.3781, capitalCosts, 1e-3);
+	}
+
+	@Test
+	public void testNoReplacementsNoResidualValue() {
+		Project project = new Project();
+		project.duration = 20;
+		project.costSettings = new CostSettings();
+		project.costSettings.interestRate = 2;
+		project.costSettings.investmentFactor = 1.03;
+
+		CostResultItem item = new CostResultItem();
+		item.costs = new ProductCosts();
+		item.costs.investment = 10_000;
+		item.costs.duration = 20;
+
+		double capitalCosts = CapitalCosts.get(item, project, 2);
+		Assert.assertEquals(611.5671, capitalCosts, 1e-3);
 	}
 
 }
