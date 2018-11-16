00a9f1717a9d1f8ff0f6a2d6633766b365d498b9
msrocka <michael.srocka@gmail.com>
2018-11-16 15:32:27 +0100

    #52 electricity sheet

--- sophena-core/src/main/java/sophena/io/excel/ElectricitySheet.java
+++ sophena-core/src/main/java/sophena/io/excel/ElectricitySheet.java
@@ -2,8 +2,6 @@ package sophena.io.excel;
 
 import java.util.Arrays;
 
-import org.apache.poi.ss.usermodel.CellStyle;
-import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 
 import sophena.calc.ProjectResult;
@@ -14,42 +12,50 @@ import sophena.model.ProducerFunction;
 
 class ElectricitySheet {
 
-	private Workbook wb;
-	private ProjectResult result;
+	private final ProjectResult result;
+	private final SheetWriter w;
 
 	ElectricitySheet(Workbook wb, ProjectResult result) {
-		this.wb = wb;
 		this.result = result;
+		w = new SheetWriter(wb, "Strom");
 	}
 
 	void write() {
-		Sheet sheet = wb.createSheet("Strom");
-		header(sheet);
-		int row = 1;
+
+		w.boldStr("Wärmeerzeuger")
+				.boldStr("Rang")
+				.boldStr("Nennleistung [kW]")
+				.boldStr("Erzeugter Strom [kWh]")
+				.boldStr("Anteil [%]")
+				.boldStr("Volllaststunden [h]")
+				.boldStr("Wirkungsgrad [%]")
+				.nextRow();
+
 		Arrays.sort(result.energyResult.producers,
 				(r1, r2) -> Integer.compare(r1.rank, r2.rank));
 		double total = calculateTotal(result.energyResult.producers);
+
 		for (Producer p : result.energyResult.producers) {
 			if (p.boiler == null || !p.boiler.isCoGenPlant)
 				continue;
 			double heat = result.energyResult.totalHeat(p);
 			double value = GeneratedElectricity.get(p, result);
-			Excel.cell(sheet, row, 0, p.name);
+			w.str(p.name);
 			if (p.function == ProducerFunction.BASE_LOAD) {
-				Excel.cell(sheet, row, 1, p.rank + " - Grundlast");
+				w.str(p.rank + " - Grundlast");
 			} else {
-				Excel.cell(sheet, row, 1, p.rank + " - Spitzenlast");
+				w.str(p.rank + " - Spitzenlast");
 			}
-			Excel.cell(sheet, row, 2, Math.round(p.boiler.maxPowerElectric));
-			Excel.cell(sheet, row, 3, Math.round(value));
-			Excel.cell(sheet, row, 4, Math.round(((value / total) * 100)));
-			Excel.cell(sheet, row, 5,
-					Math.round(Producers.fullLoadHours(p, heat)));
-			Excel.cell(sheet, row, 6,
-					Math.round(p.boiler.efficiencyRateElectric * 100d));
-			row++;
+
+			w.rint(p.boiler.maxPowerElectric)
+					.rint(value)
+					.rint((value / total) * 100)
+					.rint(Producers.fullLoadHours(p, heat))
+					.rint(p.boiler.efficiencyRateElectric * 100d)
+					.nextRow();
 		}
-		Excel.autoSize(sheet, 0, 6);
+
+		Excel.autoSize(w.sheet, 0, 6);
 	}
 
 	private double calculateTotal(Producer[] producers) {
@@ -58,17 +64,6 @@ class ElectricitySheet {
 			total += GeneratedElectricity.get(pr, result);
 		}
 		return total;
-	}
-
-	private void header(Sheet sheet) {
-		CellStyle style = Excel.headerStyle(wb);
-		Excel.cell(sheet, 0, 0, "Wärmeerzeuger").setCellStyle(style);
-		Excel.cell(sheet, 0, 1, "Rang").setCellStyle(style);
-		Excel.cell(sheet, 0, 2, "Nennleistung in kW").setCellStyle(style);
-		Excel.cell(sheet, 0, 3, "Erzeugter Strom in kWh").setCellStyle(style);
-		Excel.cell(sheet, 0, 4, "Anteil in %").setCellStyle(style);
-		Excel.cell(sheet, 0, 5, "Volllaststunden in h").setCellStyle(style);
-		Excel.cell(sheet, 0, 6, "Wirkungsgrad in %").setCellStyle(style);
 	}
 
 }
