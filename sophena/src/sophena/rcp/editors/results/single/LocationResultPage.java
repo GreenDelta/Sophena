e1e0e5f151bd943001ef63c38ab107c0cf1cf17d
msrocka <michael.srocka@gmail.com>
2020-05-26 11:09:12 +0200

    use Workspace.html

--- sophena/src/sophena/rcp/editors/results/single/LocationResultPage.java
+++ sophena/src/sophena/rcp/editors/results/single/LocationResultPage.java
@@ -1,21 +1,13 @@
 package sophena.rcp.editors.results.single;
 
-import java.io.File;
-import java.io.InputStream;
-import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.forms.IManagedForm;
 import org.eclipse.ui.forms.editor.FormPage;
-import org.eclipse.ui.forms.widgets.FormToolkit;
-import org.eclipse.ui.forms.widgets.ScrolledForm;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.google.gson.Gson;
 
@@ -28,7 +20,6 @@ import sophena.rcp.utils.UI;
 public class LocationResultPage extends FormPage {
 
 	private ResultEditor editor;
-	private FormToolkit toolkit;
 
 	public LocationResultPage(ResultEditor editor) {
 		super(editor, "sophena.LocationResultPage.html", M.Locations);
@@ -37,32 +28,19 @@ public class LocationResultPage extends FormPage {
 
 	@Override
 	protected void createFormContent(IManagedForm mform) {
-		ScrolledForm form = UI.formHeader(mform, M.Locations);
-		toolkit = mform.getToolkit();
-		Composite body = UI.formBody(form, toolkit);
+		var form = UI.formHeader(mform, M.Locations);
+		var tk = mform.getToolkit();
+		var body = UI.formBody(form, tk);
 		body.setLayout(new FillLayout());
 		var browser = new Browser(body, SWT.NONE);
-		UI.onLoaded(browser, getUrl(), () -> {
+		var url = Workspace.html("LocationResultPage",
+				() -> getClass().getResourceAsStream(
+						"LocationResultPage.html"));
+		UI.onLoaded(browser, url, () -> {
 			String json = createModel();
 			browser.execute("setData(" + json + ")");
 		});
 		form.reflow(true);
-	}
-
-	private String getUrl() {
-		String pageName = "LocationResultPage.html";
-		File f = new File(Workspace.dir(), pageName);
-		try {
-			if (!f.exists()) {
-				InputStream is = getClass().getResourceAsStream(pageName);
-				Files.copy(is, f.toPath());
-			}
-			return f.toURI().toURL().toString();
-		} catch (Exception e) {
-			Logger log = LoggerFactory.getLogger(getClass());
-			log.error("Could not get URL to location page", e);
-			return "";
-		}
 	}
 
 	private String createModel() {
