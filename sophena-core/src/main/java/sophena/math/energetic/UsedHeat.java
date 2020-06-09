3b42e803c74f8a2973d497e01ef40a87b4772a79
msrocka <michael.srocka@gmail.com>
2020-06-09 15:33:50 +0200

    see #93: remove buffer loss from used heat

--- sophena-core/src/main/java/sophena/math/energetic/UsedHeat.java
+++ sophena-core/src/main/java/sophena/math/energetic/UsedHeat.java
@@ -10,8 +10,9 @@ public class UsedHeat {
 	public static double get(ProjectResult result) {
 		if (result == null || result.energyResult == null)
 			return 0;
-		double producedHeat = result.energyResult.totalProducedHeat;
-		double distributionLoss = result.energyResult.heatNetLoss;
-		return producedHeat - distributionLoss;
+		var r = result.energyResult;
+		return r.totalProducedHeat
+				- r.heatNetLoss
+				- r.totalBufferLoss;
 	}
 }
