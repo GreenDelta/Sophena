4e82ed477208625f13580d503def3f943ee9e6db
msrocka <michael.srocka@gmail.com>
2025-07-09 17:48:09 +0200

    add up biogas amounts

--- sophena/src/sophena/rcp/editors/biogas/plant/MethaneChart.java
+++ sophena/src/sophena/rcp/editors/biogas/plant/MethaneChart.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.eclipse.draw2d.LightweightSystem;
 import org.eclipse.nebula.visualization.xygraph.dataprovider.CircularBufferDataProvider;
 import org.eclipse.nebula.visualization.xygraph.figures.Trace;
+import org.eclipse.nebula.visualization.xygraph.figures.Trace.TraceType;
 import org.eclipse.nebula.visualization.xygraph.figures.XYGraph;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
@@ -44,64 +45,53 @@ public class MethaneChart {
 		traces.clear();
 		buffers.clear();
 
+		var ds = ProfileData.allOf(profiles);
+		double[] top = ProfileData.sum(ds);
+		double max = Stats.max(top);
+
 		// add traces
 		var colors = ColorConfig.get().groupOf(ProductType.BIOMASS_BOILER);
-		double maxValue = 0;
-		for (int i = 0; i < profiles.size(); i++) {
-			var profile = profiles.get(i);
 
-
-			double[] data = profile.getMethaneProfile();
-			maxValue = Math.max(maxValue, Stats.max(data));
+		for (var d : ds) {
 
 			var buffer = Charts.dataProvider();
-			buffer.setCurrentYDataArray(data);
+			buffer.setCurrentYDataArray(top);
 			buffers.add(buffer);
+			d.subtractFrom(top);
 
-			var color = Colors.of(colors.variant(i));
-			var trace = traceOf(profile, color, buffer);
+			var color = Colors.of(colors.variant(d.index));
+			var trace = traceOf(d.name(), color, buffer);
 			traces.add(trace);
 			graph.addTrace(trace);
 		}
 
-		double max = Stats.nextStep(maxValue, 5);
 		var y = graph.getPrimaryYAxis();
-		y.setRange(0, max);
+		y.setRange(0, Stats.nextStep(max, 5));
 	}
 
 	private Trace traceOf(
-			SubstrateProfile profile, Color color, CircularBufferDataProvider buffer
+			String name, Color color, CircularBufferDataProvider buffer
 	) {
 		var trace = new Trace(
-				getTraceName(profile),
-				graph.getPrimaryXAxis(),
-				graph.getPrimaryYAxis(),
-				buffer);
+				name, graph.getPrimaryXAxis(), graph.getPrimaryYAxis(), buffer);
 		trace.setPointStyle(Trace.PointStyle.NONE);
-		trace.setTraceType(Trace.TraceType.SOLID_LINE);
+		trace.setTraceType(TraceType.AREA);
 		trace.setTraceColor(color);
-		trace.setLineWidth(2);
 		return trace;
-	}
-
-	private String getTraceName(SubstrateProfile profile) {
-		if (profile == null || profile.substrate == null)
-			return "Unknown";
-		return profile.substrate.name;
 	}
 
 	private XYGraph createGraph(LightweightSystem lws) {
 		var g = new XYGraph();
 		lws.setContents(g);
 		g.setShowTitle(false);
-		g.setShowLegend(true);
+		g.setShowLegend(false);
 		g.setBackgroundColor(Colors.getWhite());
 
 		var x = g.getPrimaryXAxis();
 		x.setRange(0, Stats.HOURS);
-		x.setTitle("Hours");
 		x.setMajorGridStep(500);
 		x.setMinorTicksVisible(false);
+		x.setTitle("");
 
 		var y = g.getPrimaryYAxis();
 		y.setTitle("m³ CH₄");
@@ -111,6 +101,45 @@ public class MethaneChart {
 		y.setFormatPattern("###,###,###,###");
 
 		return g;
+	}
+
+	private record ProfileData(int index, SubstrateProfile profile,
+														 double[] data) {
+
+		static ProfileData of(int index, SubstrateProfile profile) {
+			var data = profile.getMethaneProfile();
+			return new ProfileData(index, profile, data);
+		}
+
+		static List<ProfileData> allOf(List<SubstrateProfile> profiles) {
+			var data = new ArrayList<ProfileData>(profiles.size());
+			for (int i = 0; i < profiles.size(); i++) {
+				data.add(ProfileData.of(i, profiles.get(i)));
+			}
+			return data;
+		}
+
+		static double[] sum(List<ProfileData> data) {
+			var sum = new double[Stats.HOURS];
+			for (var d : data) {
+				for (int h = 0; h < sum.length; h++) {
+					sum[h] += d.data[h];
+				}
+			}
+			return sum;
+		}
+
+		String name() {
+			return profile.substrate != null
+					? profile.substrate.name
+					: "Unknown";
+		}
+
+		void subtractFrom(double[] ds) {
+			for (int h = 0; h < data.length; h++) {
+				ds[h] -= data[h];
+			}
+		}
 	}
 
 }
