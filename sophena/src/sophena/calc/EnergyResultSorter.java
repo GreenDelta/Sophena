187dd630197b480017d3b4165c06476bd02d52c0
msrocka <michael.srocka@gmail.com>
2026-03-31 13:09:48 +0200

    include the load in the result sorting

--- sophena/src/sophena/calc/EnergyResultSorter.java
+++ sophena/src/sophena/calc/EnergyResultSorter.java
@@ -10,37 +10,40 @@ class EnergyResultSorter {
 	static EnergyResult sort(EnergyResult result) {
 		if (result == null)
 			return null;
-		Integer[] sortedIdx = new Integer[Stats.HOURS];
+
+		var hours = new Integer[Stats.HOURS];
 		for (int i = 0; i < Stats.HOURS; i++) {
-			sortedIdx[i] = i;
+			hours[i] = i;
 		}
+
 		double[] power = result.suppliedPower;
 		double[] load = result.loadCurve;
-		Arrays.sort(sortedIdx, (i1, i2) -> {
-			double p1 = power[i1];
-			double p2 = power[i2];
-			if (Num.equal(p1, p2))
-				return -Double.compare(load[i1], load[i2]);
-			return -Double.compare(p1, p2);			
+		Arrays.sort(hours, (i, j) -> {
+			double mi = Math.max(load[i], power[i]);
+			double mj = Math.max(load[j], power[j]);
+			return Num.equal(mi, mj)
+				? -Double.compare(load[i], load[j])
+				: -Double.compare(mi, mj);
 		});
-		return createCopy(sortedIdx, result);
+		return createCopy(hours, result);
 	}
 
-	private static EnergyResult createCopy(Integer[] idx, EnergyResult r) {
-		EnergyResult s = r.clone();
-		sort(idx, r.loadCurve, s.loadCurve);
-		sort(idx, r.suppliedPower, s.suppliedPower);
+	private static EnergyResult createCopy(Integer[] hours, EnergyResult r) {
+		var s = r.clone();
+		sort(hours, r.loadCurve, s.loadCurve);
+		sort(hours, r.suppliedPower, s.suppliedPower);
 		for (int i = 0; i < r.producers.length; i++) {
-			sort(idx, r.producerResults[i], s.producerResults[i]);
+			sort(hours, r.producerResults[i], s.producerResults[i]);
 		}
-		sort(idx, r.suppliedBufferHeat, s.suppliedBufferHeat);
-		sort(idx, r.bufferCapacity, s.bufferCapacity);
+		sort(hours, r.suppliedBufferHeat, s.suppliedBufferHeat);
+		sort(hours, r.bufferCapacity, s.bufferCapacity);
 		return s;
 	}
 
-	private static void sort(Integer[] idx, double[] origin, double[] copy) {
+	private static void sort(Integer[] hours, double[] origin, double[] copy) {
+		if (origin == null || copy == null) return;
 		for (int i = 0; i < Stats.HOURS; i++) {
-			copy[i] = origin[idx[i]];
+			copy[i] = origin[hours[i]];
 		}
 	}
 }
